19ef06f0c6be123e8ffd403a4e0663f33768d8c5
shakyamoksha <Tryptamina>
2021-04-27 17:49:09 +0400

    modified products api

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+# from rest_framework.decorators import api_view
+from .products import products
 
 # Create your views here.
 
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/products',
+        '/api/products/create/',
+        
+        '/api/products/upload/',
+        '/api/products/<id>/reviews',
+        
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>',
+        '/api/products/<update>/<id>',
+    ]
+    return JsonResponse(routes, safe=False)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
