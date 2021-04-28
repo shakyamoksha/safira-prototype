22ea5fc220c3a458769701ebf1f61ea2e3462ad5
shakyamoksha <Tryptamina>
2021-04-29 00:18:59 +0400

    kaka fanner redux

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+import {addToCart} from "../actions/cartActions";
 
-export default  function CartScreen(props) {
+export default  function CartScreen({match, location, history}) {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    const dispatch = useDispatch()
+
+    React.useEffect(() => {
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
